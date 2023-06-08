25e3800463f4da311142ee5187571e4f1995d36d
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-06-08 20:40:48 +0300

    completed task 1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var button = document.querySelector("button");
-var input1 = document.querySelector("num1");
-var input2 = document.querySelector("num1");
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
 function add(num1, num2) {
     return num1 + num2;
 }
